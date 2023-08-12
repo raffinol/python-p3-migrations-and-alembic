c9625cd9370f75d9d7ed7bb641b7a7fde71af313
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-12 15:47:35 -0400

    code-along complete

---
+++ lib/migrations/versions/15ab676c2de7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 15ab676c2de7
+Revises: 
+Create Date: 2023-08-12 13:07:32.705488
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '15ab676c2de7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
